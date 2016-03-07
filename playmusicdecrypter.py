d45bbd598702abafaed6d1a352d880d606ea4d70
Roland Weber <Roland.Weber@gmx.com>
2016-03-07 20:18:15 +0100

    Unencrypted files are copied and tagged as well
    If you bought an album, then it is unencrypted, v2.0 handles such a file as damaged.
    Changes are in the function decrypt_files()

--- playmusicdecrypter.py
+++ playmusicdecrypter.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python2
 
-# playmusicdecrypter - decrypt MP3 files from Google Play Music offline storage (All Access)
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+"""To do: copy front covers """
 
-__version__ = "2.0"
+__version__ = "2.1"
 
 import os, sys, struct, re, glob, optparse, time
 import Crypto.Cipher.AES, Crypto.Util.Counter
 import mutagen
 import sqlite3
+from os import rename
+from shutil import copy
 
 import superadb
 
@@ -33,14 +21,17 @@ class PlayMusicDecrypter:
         self.infile = infile
         self.source = open(infile, "rb")
 
-        # Test if source file is encrypted
-        start_bytes = self.source.read(4)
-        if start_bytes != "\x12\xd3\x15\x27":
-            raise ValueError("Invalid file format!")
-
         # Get file info
         self.database = database
         self.info = self.get_info()
+
+    def encrypted(self):
+        # Test if source file is encrypted, if not, it is (hopefully) a normal mp3 file
+        start_bytes = self.source.read(4)
+        if start_bytes != "\x12\xd3\x15\x27":
+            return False
+        else:
+            return True
 
     def decrypt(self):
         """Decrypt one block"""
@@ -57,7 +48,7 @@ class PlayMusicDecrypter:
         return cipher.decrypt(encrypted)
 
     def decrypt_all(self, outfile=""):
-        """Decrypt all blocks and write them to outfile (or to stdout if outfile in not specified)"""
+        """Decrypt all blocks and write them to outfile (or to stdout if outfile is not specified)"""
         destination = open(outfile, "wb") if outfile else sys.stdout
         while True:
             decrypted = self.decrypt()
@@ -87,12 +78,12 @@ class PlayMusicDecrypter:
         """Remove invalid characters from filename"""
         return unicode(re.sub(r'[<>:"/\\|?*]', " ", filename))
 
-    def get_outfile(self):
-        """Returns output filename based on song informations"""
-        destination_dir = os.path.join(self.normalize_filename(self.info["AlbumArtist"]),
-                                       self.normalize_filename(self.info["Album"]))
-        filename = u"{TrackNumber:02d} - {Title}.mp3".format(**self.info)
-        return os.path.join(destination_dir, self.normalize_filename(filename))
+    def get_newdir(self): #without filename
+        structure = os.path.join(self.normalize_filename(self.info["AlbumArtist"]), self.normalize_filename(self.info["Album"]))
+        return structure
+
+    def get_newname(self):
+        return self.normalize_filename(u"{TrackNumber:02d} - {Title}.mp3".format(**self.info))
 
     def update_id3(self, outfile):
         """Update ID3 tags in outfile"""
@@ -163,28 +154,34 @@ def pull_library(source_dir="/data/data/com.google.android.music/files/music", d
 
 def decrypt_files(source_dir="encrypted", destination_dir=".", database="music.db"):
     """Decrypt all MP3 files in source directory and write them to destination directory"""
+
     print("Decrypting MP3 files...")
     if not os.path.isdir(destination_dir):
         os.makedirs(destination_dir)
 
     files = glob.glob(os.path.join(source_dir, "*.mp3"))
+
     if files:
         start_time = time.time()
-        for f in files:
-            try:
-                decrypter = PlayMusicDecrypter(database, f)
-                print(u"  Decrypting file {} -> {}".format(f, decrypter.get_outfile()))
-            except ValueError as e:
-                print(u"  Skipping file {} ({})".format(f, e))
-                continue
+        for in_file in files:
+            decrypter = PlayMusicDecrypter(database, in_file)
 
-            outfile = os.path.join(destination_dir, decrypter.get_outfile())
-            if not os.path.isdir(os.path.dirname(outfile)):
-                os.makedirs(os.path.dirname(outfile))
+            out_file = os.path.join(destination_dir, decrypter.get_newdir(), decrypter.get_newname())
+            out_dir = os.path.join(destination_dir, decrypter.get_newdir())
 
-            decrypter.decrypt_all(outfile)
-            decrypter.update_id3(outfile)
-            os.remove(f)
+            print(u"  Decrypting file {} -> {}".format(in_file, out_file))
+
+            if not os.path.isdir(os.path.dirname(out_file)):
+                os.makedirs(os.path.dirname(out_file))
+
+            if decrypter.encrypted() is True:
+                decrypter.decrypt_all(out_file)
+            else:
+                copy(in_file, out_dir)
+                rename(os.path.join(out_dir, os.path.basename(in_file)), out_file)
+                
+            decrypter.update_id3(out_file)
+            os.remove(in_file)
         print("  Decryption finished ({:.1f}s)!".format(time.time() - start_time))
     else:
         print("  No files found! Exiting...")
@@ -223,7 +220,6 @@ def main():
 
     # Decrypt all MP3 files
     decrypt_files(options.library, destination_dir, options.database)
-
 
 if __name__ == "__main__":
     main()
